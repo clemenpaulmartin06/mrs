8857725dee448038f8e0b576f590893fc5ab4912
clemenpaulmartin06 <clemenpaulmartin06@gmail.com>
2018-07-18 16:40:59 +0800

    frame

--- MRS/MRS.Android/Resources/Resource.designer.cs
+++ MRS/MRS.Android/Resources/Resource.designer.cs
@@ -2204,26 +2204,8 @@ namespace MRS.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020154
-			public const int avd_hide_password_1 = 2130837844;
-			
-			// aapt resource value: 0x7f020155
-			public const int avd_hide_password_2 = 2130837845;
-			
-			// aapt resource value: 0x7f020156
-			public const int avd_hide_password_3 = 2130837846;
-			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
-			
-			// aapt resource value: 0x7f020157
-			public const int avd_show_password_1 = 2130837847;
-			
-			// aapt resource value: 0x7f020158
-			public const int avd_show_password_2 = 2130837848;
-			
-			// aapt resource value: 0x7f020159
-			public const int avd_show_password_3 = 2130837849;
 			
 			// aapt resource value: 0x7f020055
 			public const int background = 2130837589;
@@ -2966,11 +2948,11 @@ namespace MRS.Droid
 			// aapt resource value: 0x7f02014b
 			public const int notification_icon_background = 2130837835;
 			
-			// aapt resource value: 0x7f020152
-			public const int notification_template_icon_bg = 2130837842;
+			// aapt resource value: 0x7f020154
+			public const int notification_template_icon_bg = 2130837844;
 			
-			// aapt resource value: 0x7f020153
-			public const int notification_template_icon_low_bg = 2130837843;
+			// aapt resource value: 0x7f020155
+			public const int notification_template_icon_low_bg = 2130837845;
 			
 			// aapt resource value: 0x7f02014c
 			public const int notification_tile_bg = 2130837836;
@@ -2979,16 +2961,22 @@ namespace MRS.Droid
 			public const int notify_panel_notification_icon_bg = 2130837837;
 			
 			// aapt resource value: 0x7f02014e
-			public const int samuel_l_jackson = 2130837838;
+			public const int playlist = 2130837838;
 			
 			// aapt resource value: 0x7f02014f
-			public const int settings = 2130837839;
+			public const int profile = 2130837839;
 			
 			// aapt resource value: 0x7f020150
-			public const int star = 2130837840;
+			public const int samuel_l_jackson = 2130837840;
 			
 			// aapt resource value: 0x7f020151
-			public const int users = 2130837841;
+			public const int settings = 2130837841;
+			
+			// aapt resource value: 0x7f020152
+			public const int star = 2130837842;
+			
+			// aapt resource value: 0x7f020153
+			public const int users = 2130837843;
 			
 			static Drawable()
 			{
